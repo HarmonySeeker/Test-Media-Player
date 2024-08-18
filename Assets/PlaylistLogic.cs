1b779d946e7fb847748c446ffa9b2ba2141096f8
HarmonySeeker <43490370+HarmonySeeker@users.noreply.github.com>
2024-08-19 01:44:47 +0700

    rewrote videoinfo dict struct

--- Assets/PlaylistLogic.cs
+++ Assets/PlaylistLogic.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using static System.Net.WebRequestMethods;
 
 public class PlaylistLogic : MonoBehaviour
 {
@@ -11,10 +12,16 @@ public class PlaylistLogic : MonoBehaviour
     [SerializeField]
     private TMP_Text videoName;
 
-    private Dictionary<string, string> videoInfo = new Dictionary<string, string> {
-        { "https://storage.yandexcloud.net/viavr.global/CARTOONS/Peppa_Pig/Peppa_Pig_s01e40_Daddy_Gets_Fit.mp4", "1. Свинка Пеппа: 40. Папа решил стать стройным"},
-        { "https://storage.yandexcloud.net/viavr.global/CARTOONS/Peppa_Pig/Peppa_Pig_s01e41_Shopping.mp4", "2. Свинка Пеппа: 41. Супермаркет" },
-        { "https://storage.yandexcloud.net/viavr.global/CARTOONS/Peppa_Pig/Peppa_Pig_s01e38_Sleepy_Princess.mp4", "3. Свинка Пеппа: 38. Спящая принцесса" }
+    private Dictionary<int, List<string>> videoInfo = new Dictionary<int, List<string>> {
+        {0, new List<string> {"1. Свинка Пеппа: 40. Папа решил стать стройным",
+            "https://storage.yandexcloud.net/viavr.global/CARTOONS/Peppa_Pig/Peppa_Pig_s01e40_Daddy_Gets_Fit.mp4",
+            "https://storage.yandexcloud.net/viavr.global/assets/content_card/bfdebabe-a264-4b97-ae74-e6abb2bc5ce3/images/47631a5d39d3.jpg"} },
+        {1, new List<string> {"2. Свинка Пеппа: 41. Супермаркет",
+            "https://storage.yandexcloud.net/viavr.global/CARTOONS/Peppa_Pig/Peppa_Pig_s01e41_Shopping.mp4",
+            "https://storage.yandexcloud.net/viavr.global/assets/content_card/a0a9a962-9510-40b0-bad6-897722e010a4/images/f6ab6c7ffc58.jpg"} },
+        {2, new List<string> {"3. Свинка Пеппа: 38. Спящая принцесса",
+            "https://storage.yandexcloud.net/viavr.global/CARTOONS/Peppa_Pig/Peppa_Pig_s01e38_Sleepy_Princess.mp4",
+            "https://storage.yandexcloud.net/viavr.global/assets/content_card/f91aaa3e-d4b9-4118-975a-6091e8ea29b7/images/eab2500465ed.jpg"} }
     };
 
     private void Awake()
@@ -41,13 +48,14 @@ public class PlaylistLogic : MonoBehaviour
 
         // Build the playlist
 
-        foreach (KeyValuePair<string, string> infoEntry in videoInfo)
+        foreach (KeyValuePair<int, List<string>> infoEntry in videoInfo)
         {
             MediaPlaylist.MediaItem item = new MediaPlaylist.MediaItem();
             
-            item.mediaPath = new MediaPath(infoEntry.Key, MediaPathType.AbsolutePathOrURL);
+            item.mediaPath = new MediaPath(infoEntry.Value[1], MediaPathType.AbsolutePathOrURL);
             item.startMode = PlaylistMediaPlayer.StartMode.Manual;
-            item.name = infoEntry.Value;
+            item.name = infoEntry.Value[0];
+
             pmp.Playlist.Items.Add(item);
 
             if (videoName.text == "")
@@ -55,7 +63,7 @@ public class PlaylistLogic : MonoBehaviour
                 videoName.text = item.name;
             }
 
-            Debug.Log(infoEntry.Value);
+            Debug.Log(infoEntry.Value[0]);
         }
     }
 
