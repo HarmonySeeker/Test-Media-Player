c8d66dad9cdedcfa10bd761a176cf63bd489c662
HarmonySeeker <43490370+HarmonySeeker@users.noreply.github.com>
2024-08-20 16:41:36 +0700

    added preview image caching
    known bug: player doesn't show video sometimes when switching over

--- Assets/VideoGalleryManager.cs
+++ Assets/VideoGalleryManager.cs
@@ -1,6 +1,9 @@
+using RenderHeads.Media.AVProVideo;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
@@ -26,20 +29,69 @@ public class VideoGalleryManager : MonoBehaviour
 
     IEnumerator DownloadImage(string MediaUrl, Image videoThumbnail, Image loadingThumbnail)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
-        yield return request.SendWebRequest();
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        string filename = Path.GetFileName(new Uri(MediaUrl).AbsolutePath);
+        Texture2D previewTexture = GetImage(filename, 512, 256);
+
+        if (!previewTexture)
         {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            videoThumbnail.sprite = Sprite.Create(((DownloadHandlerTexture)request.downloadHandler).texture, new Rect(0, 0, 512, 256), new Vector2());
-            videoThumbnail.color = Color.white;
-            loadingThumbnail.enabled = false;
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(request.error);
+                yield break;
+            }
+            else
+            {
+                previewTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                SaveImage(previewTexture, filename);
+            }
         }
 
+        videoThumbnail.sprite = Sprite.Create(previewTexture, new Rect(0, 0, 512, 256), new Vector2());
+        videoThumbnail.color = Color.white;
+        loadingThumbnail.enabled = false;
+    }
 
+    private Texture2D GetImage(string fileName, int width = 1, int height = 1)
+    {
+        string savePath = Application.persistentDataPath;
+
+        try
+        {
+            if (File.Exists(savePath + fileName))
+            {
+                byte[] bytes = File.ReadAllBytes(Path.Combine(savePath, fileName));
+                Texture2D texture = new Texture2D(width, height);
+                texture.LoadRawTextureData(bytes);
+                Debug.Log($"Loading preview texture from {Path.Combine(savePath, fileName)}");
+                return texture;
+            }
+
+            return null;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    private static void SaveImage(Texture2D image, string filename)
+    {
+        string savePath = Application.persistentDataPath;
+        try
+        {
+            if (!Directory.Exists(savePath))
+            {
+                Directory.CreateDirectory(savePath);
+            }
+            File.WriteAllBytes(Path.Combine(savePath, filename), image.GetRawTextureData());
+            Debug.Log($"Saving preview texture to {Path.Combine(savePath, filename)}");
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
     }
 
     public void InitializeVideo((int, Sprite) value)
